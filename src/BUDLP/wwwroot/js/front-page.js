089d21986219d0f8d146b764b5797c5a29eb2143
Benjamin Corn <bcorn@bu.edu>
2016-10-15 11:32:44 -0400

    Front Page Master Topic Toggle
    Added master toggle on front page for allowing users to toggle all
    topics on / off.

--- src/BUDLP/wwwroot/js/front-page.js
+++ src/BUDLP/wwwroot/js/front-page.js
@@ -1,13 +1,33 @@
 ﻿$(function () {
-    $('.ui.accordion')
-        .accordion()
-    ;
 
+    // Init all dropdowns to Semantic UI dropdown
     $('.ui.dropdown')
         .dropdown()
     ;
 
+    // Front-page login button toggle login modal
     $('.login-modal')
         .modal('attach events', '.login-button')
     ;
+
+    // Front-page topic toggle all
+    $('.master.topic.checkbox')
+  .checkbox({
+      // check all children
+      onChecked: function () {
+          var
+            $childCheckbox = $('.table .checkbox');
+          ;
+          $childCheckbox.checkbox('check');
+      },
+      // uncheck all children
+      onUnchecked: function () {
+          var
+            $childCheckbox = $('.table .checkbox');
+          ;
+          $childCheckbox.checkbox('uncheck');
+      }
+  })
+    ;
 });
+

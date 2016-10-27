5cef3ea6cc838643193de05b66cee9a2beb34bbe
Benjamin Corn <bcorn@bu.edu>
2016-10-27 00:06:27 -0400

    Code Commenting and Refactoring - Front Page Controller (Angular) and UPC

--- src/BUDLP/APIControllers/UserProfileController.cs
+++ src/BUDLP/APIControllers/UserProfileController.cs
@@ -38,15 +38,6 @@ namespace BUDLP.APIControllers
 
             if (result.Succeeded)
             {
-                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
-                // Send an email with this link
-                //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
-                //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
-                //    $"Please confirm your account by clicking this link: <a href='{callbackUrl}'>link</a>");
-                //await _signInManager.SignInAsync(user, isPersistent: false);
-                //_logger.LogInformation(3, "User created a new account with password.");
-                //return RedirectToLocal(returnUrl);
 
                 foreach (var top in payload.ProfileTopics)
                 {
--- src/BUDLP/wwwroot/js/angular/frontPageController.js
+++ src/BUDLP/wwwroot/js/angular/frontPageController.js
@@ -42,35 +42,45 @@
     // code for controller itself
     function frontPageController($http, $timeout, $scope) {
 
-        // Init all dropdowns to Semantic UI dropdown
+        // Reference to scope of controller
         var vm = this;
 
-
-        // Front-page login button toggle login modal
+        // On page load book-keeping
         $(function () {
             vm.Topics = {};
             vm.UserProfile = {};
             vm.UserProfile.TargetLanguage = 1;
+
+            // Init dropdown to C
             $('.ui.target.language.dropdown')
                 .dropdown('set selected', '1')
             ;
 
+            // Click event on login button
             $('.login-modal')
                 .modal('attach events', '.login-button')
             ;
         });
 
+        // Detect changes to target language dropdown
         $scope.$watch('vm.UserProfile.TargetLanguage', function () {
+            // Display loading spinner while we work
             $.blockUI({ message: '<div class="ui active dimmer"><div class="ui loader"></div></div>' });
+
+            // Craft payload with target language
             var payload = { Language: vm.UserProfile.TargetLanguage };
 
+            // Retrieve topic list with unique ordering
             $http.post("/api/topics/get", payload)
                         .then(function (result) {
                             vm.Topics = JSON.parse(result.data);
 
+                            // Wait for scope to digest our t(r)opical food
                             $timeout(function () {
+                                // Init dropdown and set default to none
                                 $('.ui.dropdown.experience').dropdown('set selected', 'None');
                             })
+                            // Let'em roam free again
                             $.unblockUI();
                         },
                         function () {
@@ -81,7 +91,9 @@
                         });
         })
 
-        vm.CreateProfile = function(){
+        // Function for click event on sign up form
+        vm.CreateProfile = function () {
+            // Craft payload object for consumption
             var payload = {
                 FullName: vm.UserProfile.FullName,
                 Email: vm.UserProfile.Email,
@@ -90,12 +102,18 @@
                 TargetLanguage: vm.UserProfile.TargetLanguage
             };
 
+                // Request an account to be created
                 $http.post("/api/profile/create", payload)
                     .then(function (result) {
+                        // Reset profile form
                         vm.UserProfile = {};
+
+                        // Hide login modal
                         $('.login-modal')
                             .modal('hide')
                         ;
+
+                        // Teleport user to their custom course
                         window.location.href = "/course";
                     },
                     function () {

2af62085ede38d07728a5f961c58e9a45cf4faa4
Benjamin Corn <bcorn@bu.edu>
2017-11-04 00:20:51 -0400

    Restyled widths of front-page elements to be fully responsive.

--- src/BUDLP/APIControllers/CoursesController.cs
+++ src/BUDLP/APIControllers/CoursesController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Azure.Documents;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace BUDLP.APIControllers
 {
@@ -13,6 +19,12 @@ namespace BUDLP.APIControllers
         private ApplicationDbContext _appDb;
         private RoleManager<IdentityRole> _roleManager;
         private PlatformDbContext _ctx;
+        private DocumentClient _client;
+        private static readonly string endpointUrl = "https://odlebrix.documents.azure.com:443/";
+        private static readonly string authorizationKey = "zlUgYbN7ICMazassuP6NBvnxtHQHFc50UjvR0480TUizOW9mCJ1m4rMU99UUz0n38oCf4qI51OegKdB15t1jJQ==";
+        private static readonly string databaseId = "odle-courses";
+        private static readonly string courseCollection = "Courses";
+        private static readonly string progressCollection = "Progress";
 
         public CoursesController(PlatformDbContext ctx, ApplicationDbContext appDb, UserManager<AuthenticatedUser> userManager, RoleManager<IdentityRole> roleManager)
         {
@@ -20,13 +32,43 @@ namespace BUDLP.APIControllers
             _ctx = ctx;
             _appDb = appDb;
             _roleManager = roleManager;
+            _client = new DocumentClient(new Uri(endpointUrl), authorizationKey);
         }
 
-        // Front page topics ordered by target language
         [HttpGet("api/v2/courses")]
         public JsonResult GetCourses()
         {
-            return new JsonResult("Hello");
+            var courseLink = UriFactory.CreateDocumentCollectionUri(databaseId, courseCollection);        
+            var response = _client.CreateDocumentQuery(courseLink).ToList();
+            return new JsonResult(response);
         }
+
+        [HttpGet("api/v2/courses/{courseId}")]
+        public JsonResult GetCoursesById(int courseId)
+        {
+            var courseLink = UriFactory.CreateDocumentCollectionUri(databaseId, courseCollection);
+            var response = _client.CreateDocumentQuery(courseLink, "SELECT * FROM c where c.courseId = " + courseId).ToList();
+            return new JsonResult(response);
+        }
+
+        [HttpGet("api/v2/progress")]
+        public async Task<JsonResult> GetProgress()
+        {
+            var user = await GetCurrentUserAsync();
+            var courseLink = UriFactory.CreateDocumentCollectionUri(databaseId, progressCollection);
+            var response = _client.CreateDocumentQuery(courseLink, "SELECT * FROM p where c.learnerId = " + user.Email).ToList();
+            return new JsonResult(response);
+        }
+
+        [HttpGet("api/v2/progress/{learnerId}")]
+        public JsonResult GetProgressById(string learnerId)
+        {
+            var courseLink = UriFactory.CreateDocumentCollectionUri(databaseId, progressCollection);
+            var response = _client.CreateDocumentQuery(courseLink, "SELECT * FROM p where p.learnerId = " + learnerId).ToList();
+            return new JsonResult(response);
+        }
+
+        private Task<AuthenticatedUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
     }
 }
